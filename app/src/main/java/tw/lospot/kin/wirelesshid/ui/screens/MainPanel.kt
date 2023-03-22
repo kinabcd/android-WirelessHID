52397ff401794a15fdfb5388d82d62a93ae6aeee
Kin Lo <kinabcd@gmail.com>
2023-03-22 13:25:23 +0800

    show selected device name on main panel

--- app/src/main/java/tw/lospot/kin/wirelesshid/ui/screens/MainPanel.kt
+++ app/src/main/java/tw/lospot/kin/wirelesshid/ui/screens/MainPanel.kt
@@ -1,24 +1,30 @@
 package tw.lospot.kin.wirelesshid.ui.screens
 
+import android.bluetooth.BluetoothManager
+import android.content.Context.BLUETOOTH_SERVICE
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.systemGestureExclusion
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import tw.lospot.kin.wirelesshid.R
 import tw.lospot.kin.wirelesshid.ui.PageButton
 import tw.lospot.kin.wirelesshid.ui.SETTINGS
 import tw.lospot.kin.wirelesshid.ui.UiModel
 import tw.lospot.kin.wirelesshid.ui.keyboard.QwertKeyboard
 import tw.lospot.kin.wirelesshid.ui.mouse.TouchPad
+import tw.lospot.kin.wirelesshid.util.Permission
 
 @Composable
 fun MainPanelScreen(
@@ -75,9 +81,25 @@ fun MainPanelScreen(
                 }
             }
         }
+        val context = LocalContext.current
+        var selectedDeviceName by remember { mutableStateOf("None") }
+        LaunchedEffect(model.selectedDevice) {
+            launch(Dispatchers.Default) {
+                val bluetoothManager =
+                    (context.getSystemService(BLUETOOTH_SERVICE) as BluetoothManager)
+                selectedDeviceName = model.selectedDevice?.let {
+                    if (Permission.checkBluetoothConnectPermission(context)) {
+                        bluetoothManager.adapter.getRemoteDevice(it)?.name
+                    } else it
+                } ?: "None"
+            }
+        }
         Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.height(52.dp)) {
             Spacer(modifier = Modifier.width(32.dp))
-            Text(text = model.state.name, modifier = Modifier.weight(1f))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = selectedDeviceName, modifier = Modifier.fillMaxWidth())
+                Text(text = model.state.name, modifier = Modifier.fillMaxWidth(), fontSize = 7.sp)
+            }
             Spacer(modifier = Modifier.width(16.dp))
             PageButton(
                 painter = painterResource(id = R.drawable.ic_power),
